3a638086c0caed20d2e59857f6ebe37713285446
zazuwka <zazuwka@yahoo.com>
2023-06-22 01:07:01 +0000

    added new files

---
+++ Class3/variable.tf
@@ -0,0 +1,17 @@
+variable region {
+  type = string
+  description = "Enter region"
+  default = ""
+}
+
+variable key_name {
+    type = string
+    default = ""
+    description = "Provide key pair name"
+}
+
+variable key_file {
+    type = string
+    description = "Provide key file location"
+    default = "~/.ssh/id_rsa.pub"
+}
